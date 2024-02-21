fb2a35f15035c508855badc77af16ff5a3d7a723
mohamedhadji1 <100348710+mohamedhadji1@users.noreply.github.com>
2024-02-21 01:54:56 +0100

    a

--- AngularPIFront/src/app/app-routing.module.ts
+++ AngularPIFront/src/app/app-routing.module.ts
@@ -2,15 +2,7 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlankComponent } from './layouts/blank/blank.component';
 import { FullComponent } from './layouts/full/full.component';
-import { AppSideLoginComponent } from './pages/authentication/login/login.component';
 import { NotfoundComponent } from './notfound/notfound.component';
-import { CompanyfrontComponent } from './pages/client/components/companyfront/companyfront.component';
-import { DefensefrontComponent } from './fronttemplate/defensefront/defensefront.component';
-import { InternshipfrontComponent } from './fronttemplate/internshipfront/internshipfront.component';
-import { TaskfrontComponent } from './fronttemplate/taskfront/taskfront.component';
-import { EventfrontComponent } from './pages/client/components/eventfront/eventfront.component';
-import { InterviewfrontComponent } from './fronttemplate/interviewfront/interviewfront.component';
-import { ComplaintfrontComponent } from './pages/client/components/complaintfront/complaintfront.component';
 import { AddTaskComponent } from './pages/ui-components/task/add-task/add-task.component';
 
 const routes: Routes = [
