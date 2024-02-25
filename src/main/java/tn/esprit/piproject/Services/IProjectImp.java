b805b006f2ea4247db0a88f1673040b8f5193c5d
ahmedbdllh <103193085+ahmedbdllh@users.noreply.github.com>
2024-02-25 12:06:56 +0100

    ahmed

--- src/main/java/tn/esprit/piproject/Services/IProjectImp.java
+++ src/main/java/tn/esprit/piproject/Services/IProjectImp.java
@@ -1,20 +1,35 @@
 package tn.esprit.piproject.Services;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import tn.esprit.piproject.Entities.Documents;
+import tn.esprit.piproject.Entities.Internship;
+import tn.esprit.piproject.Entities.Task;
 import tn.esprit.piproject.Entities.User;
+import tn.esprit.piproject.Repositories.DocumentsRepository;
+import tn.esprit.piproject.Repositories.InternshipRepository;
+import tn.esprit.piproject.Repositories.TaskRepository;
 import tn.esprit.piproject.Repositories.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
+
 @Service
-@AllArgsConstructor
 @NoArgsConstructor
-public class IProjectImp implements IProjectService{
+@AllArgsConstructor
+public class IProjectImp implements IProjectService {
+
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private TaskRepository taskRepository;
+    @Autowired
+    private InternshipRepository internshipRepository;
+    @Autowired
+    private DocumentsRepository documentsRepository;
 
     @Override
     public List<User> getAllUsers() {
@@ -40,4 +55,102 @@ public class IProjectImp implements IProjectService{
     public void deleteUser(int id) {
         userRepository.deleteById(id);
     }
+/*********************************************************/
+    @Override
+    public List<Internship> getAllinternships() {
+        return internshipRepository.findAll();
+    }
+
+    @Override
+    public Optional<Internship> getinternshipById(int id) {
+        return internshipRepository.findById(id);
+    }
+
+    @Override
+    public Internship createInternship(Internship internship) {
+        return internshipRepository.save(internship);
+    }
+
+    @Override
+    public Internship updateinternship(Internship internship) {
+        return internshipRepository.save(internship);
+    }
+
+    @Override
+    public void deleteinternship(int id) {
+        internshipRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Documents> getAlldocuments() {
+        return documentsRepository.findAll();
+    }
+
+    @Override
+    public Optional<Documents> getdocumentsById(int id) {
+        return documentsRepository.findById(id);
+    }
+
+    @Override
+    public Documents createdocuments(Documents documents) {
+        return documentsRepository.save(documents);
+    }
+
+    @Override
+    public Documents updatedocuments(Documents documents) {
+        return documentsRepository.save(documents);
+    }
+
+    @Override
+    public void deletedocuments(int id) {
+        documentsRepository.deleteById(id);
+
+    }
+
+    @Override
+    public List<Task> getAllTasks() {return taskRepository.findAll();}
+
+    @Override
+    public Optional<Task> getTaskById(int id) {return taskRepository.findById(id);}
+
+    @Override
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public void deleteTask(int id) {
+        taskRepository.deleteById(id);
+    }
+    /***************************************************/
+    /*
+    @Override
+    public List<Documents> getAllDocuments() {
+        return documentsRepository.findAll();
+    }
+
+    @Override
+    public Optional<Documents> getDocumentsById(int id) {
+        return DocumentsRepository.findById(id);
+    }
+
+    @Override
+    public User createDocument(Documents documents) {
+        return documentsRepository.save(documents);
+    }
+
+    @Override
+    public User updateDocuments(Documents documents) {
+        return userRepository.save(documents);
+    }
+
+    @Override
+    public void deleteDocuments(int id) {
+        documentsRepository.deleteById(id);
+    }*/
 }
