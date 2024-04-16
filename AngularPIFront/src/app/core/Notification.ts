ff55a2a7921ef8ccbbcb4239967f4c2dc948e58f
mohamedhadji1 <100348710+mohamedhadji1@users.noreply.github.com>
2024-04-16 12:58:25 +0100

    merge

--- AngularPIFront/src/app/core/Notification.ts
+++ AngularPIFront/src/app/core/Notification.ts
@@ -1,15 +1,5 @@
-import { User } from "./User";
 
-export enum NotificationStatus {
-    READ = 'read',
-    UNREAD = 'unread'
-  }
-  
-  export interface Notification {
-    id: number;
-    sender: User;
-    recipient: User;
-    message: string;
-    timestamp: Date;
-    status: NotificationStatus;
-  }
+export interface notification {
+  description : String ;
+  user : String ;
+}
